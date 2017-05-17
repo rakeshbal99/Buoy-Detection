e8b4b9fca4652d3a8a19e99a8b3d0890182d1a43
Ayu <asayushi789@gmail.com>
2017-05-18 00:39:18 +0530

    CFAR UPDATED

--- src/cfarAlgo.cpp
+++ src/cfarAlgo.cpp
@@ -8,7 +8,7 @@
 using namespace cv;
 using namespace std;
 
-Mat cfar(Mat& frame, Mat img, int num_train, int num_guard, float rate1, float rate2, int block_size) {
+Mat cfar(Mat& frame, Mat img, int num_train, int num_guard, float rate1, int block_size) {
 
   Mat a(img.rows, img.cols, CV_8UC3, Scalar(0,0,0));
   
@@ -16,35 +16,15 @@ Mat cfar(Mat& frame, Mat img, int num_train, int num_guard, float rate1, float r
   int num_cols = a.cols - (a.cols%num_train) ;
   int num_side = num_train/2;
   
-  double alpha1 = num_train * ( pow(rate1, -1.00/num_train) -1 );	
-  double alpha2 = num_train * ( pow(rate2, -1.00/num_train) -1 );  
+ 	
+  double alpha1 = num_train * ( pow(rate1, -1.00/num_train) -1 );  
 
-  for(int i = num_side; i <= num_rows; i += num_side*2) 
-    for(int j = num_side; j <= num_cols; j += num_side*2) {
+  for(int i = num_side; i <= num_rows; i += block_size) 
+    for(int j = num_side; j <= num_cols; j += block_size) {
        
        int sum1 = 0, sum2 = 0;
        double thresh, p_noise;
-      
-	//CASO-CFAR
-       for(int x = i-num_side; x <= i-num_guard; x++) 
-         for(int y = j-num_side; y <= j-num_guard; y++)
-           sum1 += img.at<Vec3b>(x,y)[0];
 
-       for(int x = i+num_guard; x <= i+num_side; x++)
-         for(int y = j+num_guard; y <= j+num_side; y++)
-           sum2 += img.at<Vec3b>(x,y)[0];
-  	
-       p_noise=min(sum1,sum2)/(num_train*num_train);
-       thresh = alpha1*p_noise;
-
-       if( img.at<Vec3b>(i,j)[0] < thresh) {
-
-         for(int k = i-block_size/2; k <= i+block_size/2; k++)
-           for(int l = j-block_size/2; l <= j+block_size/2; l++)
-             a.at<Vec3b>(k,l)[0] = 255;	
-	}
-
-	//CA-CFAR 
        for(int x = i-(num_guard/2); x <= i+(num_guard/2)+1; x++)
          for(int y = j-(num_guard/2); y <= j+(num_guard/2)+1; y++)
            sum1 += img.at<Vec3b>(x,y)[0];
@@ -54,7 +34,7 @@ Mat cfar(Mat& frame, Mat img, int num_train, int num_guard, float rate1, float r
            sum2 += img.at<Vec3b>(x,y)[0];
   
        p_noise = fabs(sum1-sum2)/(num_train*num_train);
-       thresh = alpha2*p_noise;
+       thresh = alpha1*p_noise;
   
        if( img.at<Vec3b>(i,j)[0] > thresh) {
 
@@ -82,7 +62,7 @@ int main() {
   
       cvtColor(frame, hsv, COLOR_BGR2HSV);
 
-      Mat image = cfar(frame, hsv, 6, 3, 0.5, 0.2, 3);
+      Mat image = cfar(frame, hsv, 8, 6, 0.01, 3);
       cvtColor(image, grey, COLOR_BGR2GRAY);
    
       imshow("cfar_output", image);    
