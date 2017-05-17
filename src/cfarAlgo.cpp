3a726686e71c4fb0e617a1bc555236d5769ca224
Ayu <asayushi789@gmail.com>
2017-05-18 00:03:02 +0530

    CFAR UPDATED

--- src/cfarAlgo.cpp
+++ src/cfarAlgo.cpp
@@ -4,7 +4,6 @@
 #include <opencv2/opencv.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/segmentation.hpp"
 
 using namespace cv;
 using namespace std;
@@ -13,9 +12,9 @@ Mat cfar(Mat& frame, Mat img, int num_train, int num_guard, float rate1, float r
 
   Mat a(img.rows, img.cols, CV_8UC3, Scalar(0,0,0));
   
-  int num_rows = a.rows-(a.rows%(num_train + num_guard));
-  int num_cols = a.cols-(a.cols%(num_train + num_guard));
-  int num_side = (num_train/2)+(num_guard/2);
+  int num_rows = a.rows - (a.rows%num_train) ;
+  int num_cols = a.cols - (a.cols%num_train) ;
+  int num_side = num_train/2;
   
   double alpha1 = num_train * ( pow(rate1, -1.00/num_train) -1 );	
   double alpha2 = num_train * ( pow(rate2, -1.00/num_train) -1 );  
@@ -66,25 +65,6 @@ Mat cfar(Mat& frame, Mat img, int num_train, int num_guard, float rate1, float r
  
   }
 
-/* AVERAGE 
-
-    int count=0;
-    int sum_x=0 ; int sum_y = 0;
-
-    for(int i=0; i<a.rows; i++)
-      for(int j=0; j<a.cols; j++) {
-
-	if( a.at<Vec3b>(i,j)[0]==255 || a.at<Vec3b>(i,j)[1]==255 ) {
-	
-		sum_x+=i;
-		sum_y+=j;
-		count++;
-	}		
-     }
-	if(count!=0)
-    circle(frame, Point(sum_x/count, sum_y/count), 100,Scalar(0,0,255));
-*/
-
   return a;
 }
 
@@ -92,51 +72,24 @@ int main() {
 
   VideoCapture cap("/home/ayu/Videos/buoy.avi");
   
-  if(!cap.isOpened()){
+  if(!cap.isOpened()) {
     cout<<"Can't open video file.";
     return -1;
   }
     Mat frame, hsv, grey,res;
-  while(cap.read(frame)) {
   
-    cvtColor(frame, hsv, COLOR_BGR2HSV);
-
-    Mat image = cfar(frame, hsv, 9, 2, 0.5, 0.2, 10);
-    cvtColor(image, grey, COLOR_BGR2GRAY);
+    while(cap.read(frame)) {
   
-    dilate(grey, grey, 0, Point(-1, -1), 5, 1, 1);
+      cvtColor(frame, hsv, COLOR_BGR2HSV);
 
-  /* HOUGH CIRCLES
-
-    vector<Vec3f> circles;
-    HoughCircles( grey, circles, CV_HOUGH_GRADIENT, 1, 5, 1, 100, 10, 0 );
-  
-    for( size_t i = 0; i < circles.size(); i++ ) {
-      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-      int radius = cvRound(circles[i][2]);
-      // circle center
-      circle( frame, center, 3, Scalar(0,255,0), -1, 8, 0 );
-      // circle outline
-      circle( frame, center, radius, Scalar(0,0,255), 3, 8, 0 );
-    }
-  */
-
-  /* CONTOUR DETECTIOIN
-    
-    vector< vector< Point > > contours;
-    findContours(grey, contours,CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-
-    for (size_t idx = 0; idx < contours.size(); idx++) {
-        drawContours(frame, contours, idx, Scalar(0));
-    }
-  */
-    imshow("cfar_output", image);    
-    imshow("object", frame);
-    pyrMeanShiftFiltering( image, res, 20, 20, 6);
-    imshow("result", res);
+      Mat image = cfar(frame, hsv, 6, 3, 0.5, 0.2, 3);
+      cvtColor(image, grey, COLOR_BGR2GRAY);
+   
+      imshow("cfar_output", image);    
+      imshow("object", frame);
  	   
-    if(waitKey(1)=='q')
-	break;
+      if(waitKey(1)=='q')
+      	break;
    
   }
 
